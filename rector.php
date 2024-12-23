26005512b671cb599a4aa98c77d6a2d0c6c86d95
Marek Víger <marek.viger@gmail.com>
2024-12-23 13:51:19 +0100

    added BCValueResolverInterface for compatibility between ValueResolverInterface introduced in 6.2 and ArgumentValueResolverInterface removed in 7.0

--- rector.php
+++ rector.php
@@ -18,9 +18,6 @@ return RectorConfig::configure()
     //->withPhpSets()
     //->withSymfonyContainerXml(__DIR__ . '/var/cache/dev/App_KernelDevDebugContainer.xml')
     ->withSets([
-        SymfonySetList::SYMFONY_60,
-        SymfonySetList::SYMFONY_61,
-        //SymfonySetList::SYMFONY_62,
         //SymfonySetList::SYMFONY_63,
         //SymfonySetList::SYMFONY_64,
         //SymfonySetList::SYMFONY_70,
