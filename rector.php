edb3c328a33e848af2f98b13bb40b2733bc688dc
Marek Víger <marek.viger@gmail.com>
2024-12-23 13:53:23 +0100

    rector process up to Symfony 7.2

--- rector.php
+++ rector.php
@@ -18,9 +18,8 @@ return RectorConfig::configure()
     //->withPhpSets()
     //->withSymfonyContainerXml(__DIR__ . '/var/cache/dev/App_KernelDevDebugContainer.xml')
     ->withSets([
-        //SymfonySetList::SYMFONY_70,
-        //SymfonySetList::SYMFONY_71,
-        //SymfonySetList::SYMFONY_72,
+        //SymfonySetList::SYMFONY_73,
+        //SymfonySetList::SYMFONY_74,
         //SymfonySetList::SYMFONY_CODE_QUALITY,
         //SymfonySetList::SYMFONY_CONSTRUCTOR_INJECTION,
     ])
